6fac8e4be9984ffd8d9e307ead62ed991226685c
Jonathan Tamir <jtamir@eecs.berkeley.edu>
2015-04-06 18:47:23 -0700

    rewrite to use np.array (2X performance over np.matrix)

--- src/gen_FSEmatrix.py
+++ src/gen_FSEmatrix.py
@@ -12,14 +12,14 @@ def gen_FSEmatrix(N, angles_rad, ETL, e2s, TE, T1vals, T2vals):
   T1 = np.sort(T1vals)
   T2 = np.sort(T2vals)
 
-  X = np.matrix(np.zeros((T, N * len(T1))), dtype=np.complex64)
+  X = np.zeros((T, N * len(T1)), dtype=np.complex64)
 
   c = 0
   total = N * len(T1)
   for T2 in T2vals:
     for T1 in T1vals:
       print str(c + 1) + "/" + str(total)
-      X[:, c] = FSE_signal(angles_rad, TE, T1, T2)
+      X[:, c] = FSE_signal(angles_rad, TE, T1, T2)[:,0]
       c = c + 1
 
   keep = range(1 + e2s, e2s + ETL + 1)
